8363624791b95cf69d5419f1268caf1a4aff27bf
Sofya Salova <ssalova04@gmail.com>
2026-04-04 12:22:17 +0300

    refactor: address reviewer comments - move validation to services, remove storage from controllers, use @Value

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,86 +1,32 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Value;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Value
 public class Film {
-    private Integer id;
-    private String name;
-    private String description;
-    private LocalDate releaseDate;
-    private Integer duration;
-    private Set<Integer> likes;
+    Integer id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    Integer duration;
 
-    public Film() {
-    }
+    @JsonIgnore
+    Set<Integer> likes;
 
     public Film(Integer id, String name, String description, LocalDate releaseDate, Integer duration) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.releaseDate = releaseDate;
-        this.duration = duration;
-        this.likes = new HashSet<>();
+        this(id, name, description, releaseDate, duration, new HashSet<>());
     }
 
-    public Film(Integer id, String name, String description, LocalDate releaseDate, Integer duration,
-                Set<Integer> likes) {
+    public Film(Integer id, String name, String description, LocalDate releaseDate, Integer duration, Set<Integer> likes) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.releaseDate = releaseDate;
         this.duration = duration;
         this.likes = likes != null ? likes : new HashSet<>();
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(LocalDate releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public Integer getDuration() {
-        return duration;
-    }
-
-    public void setDuration(Integer duration) {
-        this.duration = duration;
-    }
-
-    @JsonIgnore
-    public Set<Integer> getLikes() {
-        return likes;
-    }
-
-    public void setLikes(Set<Integer> likes) {
-        this.likes = likes;
     }
 }
