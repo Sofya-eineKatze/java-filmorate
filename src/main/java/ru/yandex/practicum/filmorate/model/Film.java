f56519ad51698b763a3dc048e2d1eaef50a659a1
Sofya Salova <ssalova04@gmail.com>
2026-04-04 13:21:37 +0300

    fix: add @NoArgsConstructor(force=true) for Jackson deserialization

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,12 +2,14 @@ package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Value
+@NoArgsConstructor(force = true)
 @AllArgsConstructor
 public class Film {
     Integer id;
