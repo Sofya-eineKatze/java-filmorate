9f0f9510beabc87393848bc399310930a4371863
Sofya Salova <ssalova04@gmail.com>
2026-04-04 12:35:37 +0300

    fix: working version with Java 21, no Lombok

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,24 +1,29 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Value;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-@Value
 public class User {
-    Integer id;
-    String email;
-    String login;
-    String name;
-    LocalDate birthday;
+    private Integer id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+    private Set<Integer> friends;
 
-    @JsonIgnore
-    Set<Integer> friends;
+    public User() {
+        this.friends = new HashSet<>();
+    }
 
     public User(Integer id, String email, String login, String name, LocalDate birthday) {
-        this(id, email, login, name, birthday, new HashSet<>());
+        this.id = id;
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+        this.friends = new HashSet<>();
     }
 
     public User(Integer id, String email, String login, String name, LocalDate birthday, Set<Integer> friends) {
@@ -28,5 +33,54 @@ public class User {
         this.name = name;
         this.birthday = birthday;
         this.friends = friends != null ? friends : new HashSet<>();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    @JsonIgnore
+    public Set<Integer> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(Set<Integer> friends) {
+        this.friends = friends;
     }
 }
