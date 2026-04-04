062f9c774cdf8049f3e01544bf862e8410082fb8
Sofya Salova <ssalova04@gmail.com>
2026-04-04 12:15:15 +0300

    fix: replace Lombok with plain getters/setters

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,24 +1,27 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Value;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-@Value
 public class Film {
-    Integer id;
-    String name;
-    String description;
-    LocalDate releaseDate;
-    Integer duration;
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Integer duration;
+    private Set<Integer> likes;
 
-    @JsonIgnore
-    Set<Integer> likes;
+    public Film() {}
 
     public Film(Integer id, String name, String description, LocalDate releaseDate, Integer duration) {
-        this(id, name, description, releaseDate, duration, new HashSet<>());
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+        this.likes = new HashSet<>();
     }
 
     public Film(Integer id, String name, String description, LocalDate releaseDate, Integer duration, Set<Integer> likes) {
@@ -29,4 +32,23 @@ public class Film {
         this.duration = duration;
         this.likes = likes != null ? likes : new HashSet<>();
     }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public LocalDate getReleaseDate() { return releaseDate; }
+    public void setReleaseDate(LocalDate releaseDate) { this.releaseDate = releaseDate; }
+
+    public Integer getDuration() { return duration; }
+    public void setDuration(Integer duration) { this.duration = duration; }
+
+    @JsonIgnore
+    public Set<Integer> getLikes() { return likes; }
+    public void setLikes(Set<Integer> likes) { this.likes = likes; }
 }
